30b2e161bb9777343c736644a08bf7736cc0e423
chandanana0 <156922452+chandanana0@users.noreply.github.com>
2025-07-18 08:23:18 +0530

    Update welcomepage.py

--- welcomepage.py
+++ welcomepage.py
@@ -1,5 +1,4 @@
 import streamlit as st
-st.set_page_config(page_title="Welcome Page", layout="centered")
 st.text("Hello! Please enter your name below:")
 name = st.text_input("Your Name")
 if name:
